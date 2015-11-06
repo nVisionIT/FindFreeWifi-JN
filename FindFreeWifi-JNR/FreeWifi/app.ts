b0fb1db5c6fc6bdeaba82d59158658b55b09998e
RehanaVanSchalkwyk <rehanav@nvisionit.co.za>
2015-11-06 16:10:23 +0200

    intial

---
+++ FindFreeWifi-JNR/FreeWifi/app.ts
@@ -0,0 +1,21 @@
+﻿import {bootstrap, Component, NgFor, View} from 'angular2/angular2';
+import {HTTP_PROVIDERS, Http} from 'angular2/http';
+@Component({
+    selector: 'my-app',
+    providers: [HTTP_PROVIDERS]
+    
+})
+@View({
+        directives: [NgFor],
+        templateUrl : 'app.html' 
+    })
+export class MyApp {
+    locations: Object;
+
+    constructor(http: Http) {
+        http.get('http://www.findfreewifi.co.za/publicjson/getcities').subscribe(res => {
+            this.locations = res.data;
+        });
+    }
+    
+}
